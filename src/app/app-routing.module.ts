862e1d0a1167b8bfc9d89dcd8ec1358bdc3036c0
Gabriela <gabrielacamarchiori@gmail.com>
2023-03-30 19:25:31 -0300

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListaPensamentosComponent } from './components/pensamentos/lista-pensamentos/lista-pensamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'listaPensamento',
+    pathMatch: 'full'
+  },
+  {
+    path: 'criarPensamento',
+    component: CriarPensamentoComponent
+  },
+
+  {
+    path: 'listaPensamento',
+    component: ListaPensamentosComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pensamentos/lista-pensamentos/lista-pensamentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-pensamentos',
+  templateUrl: './lista-pensamentos.component.html',
+  styleUrls: ['./lista-pensamentos.component.css']
+})
+export class ListaPensamentosComponent {
+
+}
